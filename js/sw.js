cc1cb34b53968190fca2fc99c475f292b8427342
Letalandroid <carlossoncra@gmail.com>
2022-05-01 21:05:08 -0500

    1-05-22

--- js/sw.js
+++ js/sw.js
@@ -1,11 +1,7 @@
 const registerServiceWorker = async () => {
     if ('serviceWorker' in navigator) {
         try {
-            const registration = await navigator.serviceWorker.register(
-                './js/sw.js', { // Change
-                    scope: './',
-                }
-            );
+            const registration = await navigator.serviceWorker.register("./js/sw.js");
             if (registration.installing) {
                 console.log('Service worker installing');
             } else if (registration.waiting) {
